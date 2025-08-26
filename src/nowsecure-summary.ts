942c7c3443307e81df9614447e9a689d639f2bda
Aaron Goodfellow <agoodfellow@nowsecure.com>
2025-08-26 11:35:05 -0400

    seems like components can be undefined

--- src/nowsecure-summary.ts
+++ src/nowsecure-summary.ts
@@ -109,7 +109,7 @@ export function githubJobSummaryShort(
   const results = [
     [":white_check_mark: Pass", findingsGroupedBy.pass.length.toString()],
     [":red_circle: Fail", findingsGroupedBy.fail.length.toString()],
-    [":bricks: Dependencies", assessment.deputy.components.length.toString()],
+    [":bricks: Dependencies", assessment.deputy?.components?.length?.toString() || "0"],
   ];
 
   const formatDetail = (findings: Finding[]) =>
